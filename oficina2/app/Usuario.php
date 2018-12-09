9b433dff3ef5ce0740b105500790070538195545
olimpio <olimpiodx@gmail.com>
2018-12-09 15:36:31 -0200

    seeders 80%

---
+++ oficina2/app/Usuario.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Usuario extends Model
+{
+    //
+    protected $fillable=[
+        'usuario',
+        'chapa',
+        'senha',
+        'confirma',
+        'ativo',
+        'administrador'
+    ];
+}
