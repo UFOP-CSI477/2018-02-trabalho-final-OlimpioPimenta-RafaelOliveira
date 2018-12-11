9e80fe9b64b925b0d113f37c3e4db0730771ebd7
raffoliveira <rafaelollywer@gmail.com>
2018-12-11 17:25:42 -0200

    Atualização views Welcome e Layout

--- oficina2/resources/views/usuario/index.blade.php
+++ oficina2/resources/views/usuario/index.blade.php
@@ -1,37 +1,47 @@
-<h3>Usuário</h3>
-<img src="img/usuario.png" width="100px" height="100px">
+@extends('layout')
 
-     <div class="container-table100">
-         <div class="wrap-table100">
-            <div class="table100 ver1 m-b-110">
-              <caption align="left">Usuários</caption>
-               <table>
-                  <thead>
-                     <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">USUÁRIO</th>
-                        <th scope="col">CHAPA</th>
-                        <th scope="col">SENHA</th>
-                        <th scope="col">CONFIRMA</th>
-                        <th scope="col">ATIVO</th>
-                        <th scope="col">ADMINISTRADOR</th>
-                     </tr>
-                  </thead>
-                  <tbody>
+@section('content')
 
-                  @foreach($usuarios as $w)
-                     <tr>
-                        <td data-label="Id">{{ $w->id }}</td>
-                        <td data-label="Usuário">{{ $w->usuario }}</td>
-                        <td data-label="Chapa">{{ $w->chapa }}</td>
-                        <td data-label="Senha">{{ $w->senha }}</td>
-                        <td data-label="Confirma">{{ $w->confirma }}</td>
-                        <td data-label="Ativo">{{ $w->ativo }}</td>
-                        <td data-label="Administrador">{{ $w->administrador}}</td>
-                     </tr>
-                   @endforeach
-                 </tbody>
-               </table>
-            </div>
-          </div>
-        </div>
+
+  <div class="row">
+    <div class="col s12 m5">
+      <div class="card-panel teal">
+        <span class="white-text">Usuário</span>
+      </div>
+    </div>
+  </div>
+
+<div class="container">
+  <img src="img/usuario.png" width="100px" height="100px"><h3>Usuário</h3>
+</div>
+
+
+<table class="striped highlight centered responsive-table">
+    <thead>
+       <tr>
+          <th scope="col">ID</th>
+          <th scope="col">USUÁRIO</th>
+          <th scope="col">CHAPA</th>
+          <th scope="col">SENHA</th>
+          <th scope="col">CONFIRMA</th>
+          <th scope="col">ATIVO</th>
+          <th scope="col">ADMINISTRADOR</th>
+       </tr>
+    </thead>
+    <tbody>
+
+    @foreach($usuarios as $w)
+       <tr>
+          <td data-label="Id">{{ $w->id }}</td>
+          <td data-label="Usuário">{{ $w->usuario }}</td>
+          <td data-label="Chapa">{{ $w->chapa }}</td>
+          <td data-label="Senha">{{ $w->senha }}</td>
+          <td data-label="Confirma">{{ $w->confirma }}</td>
+          <td data-label="Ativo">{{ $w->ativo }}</td>
+          <td data-label="Administrador">{{ $w->administrador}}</td>
+       </tr>
+     @endforeach
+   </tbody>
+ </table>
+
+ @endsection
