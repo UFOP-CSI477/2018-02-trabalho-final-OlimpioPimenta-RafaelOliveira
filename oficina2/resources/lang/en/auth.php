47f4b36358549cd734ced556afc75d4e358888ff
olimpio <olimpiodx@gmail.com>
2018-12-12 01:39:06 -0200

    atualização troca de óleo e revisão dos pneus logados

--- oficina2/resources/lang/en/auth.php
+++ oficina2/resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
+    'failed' => 'Email não registrado ou incorreto.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
